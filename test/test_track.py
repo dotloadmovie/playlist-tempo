3fcc616a12648b5553bb7d30ca7ce1832fcf7a10
Dave Tickle <dave.tickle@socialfinance.org.uk>
2023-11-06 11:26:16 +0000

    feat: update paths to demo data to match live API

--- test/test_track.py
+++ test/test_track.py
@@ -10,7 +10,9 @@ def test_get_track_tempo(**kwargs):
     file = open("./fakes/data.json")
     json_body = json.load(file)
 
-    kwargs["mock"].get(f"{BASE_URL}/track", json=json_body["tracks"]["hello"])
+    kwargs["mock"].get(
+        f"{BASE_URL}/audio-features/hello", json=json_body["audio-features"][0]
+    )
 
     tempo = get_track_tempo(track_id="hello", token="helloworld")
 
