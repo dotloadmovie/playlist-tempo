a409855b1b5312137504519870ee67cf340ebf53
Dave Tickle <dave.tickle@socialfinance.org.uk>
2023-11-06 11:42:47 +0000

    feat: update entrypoint for app

--- playlist_tempo/app.py
+++ playlist_tempo/app.py
@@ -1,15 +1,20 @@
 from functools import reduce
 
+from playlist_tempo.tokens import create_token
 from playlist_tempo.playlist import get_playlist_track_ids
 from playlist_tempo.track import get_track_tempo
 
 
 def average_tempo(playlist_id):
-    ids = get_playlist_track_ids(playlist_id, "helloworld")
+    token = create_token()
+    ids = get_playlist_track_ids(playlist_id, token["access_token"])
 
     def get_tempo(id):
-        return get_track_tempo(id)
+        return get_track_tempo(id, token["access_token"])
 
-    tempos = list(ids.map(get_tempo, ids))
+    tempos = list(map(get_tempo, ids))
 
     return reduce(lambda curr, next: curr + next, tempos) / len(tempos)
+
+
+print(average_tempo("helloworld"))
